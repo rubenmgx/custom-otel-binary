4aae6690b2b707676787b8a855221aae002c5656
John Casey <jdcasey@commonjava.org>
2023-12-18 11:00:25 -0600

    Update to build otelcol binary, then add to the image
    This changes the GHA build approach to execute the builder.sh 'locally' in the pipeline, then make that available to the Dockerfile. The Dockerfile is changed to use the same configuration defaults as the 'vanilla' otelcol-contrib, and checked to make sure it'll boot.
    
    Finally, the otelcol version has been upgraded to 0.91.0.

--- builder.sh
+++ builder.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-sudo wget https://github.com/open-telemetry/opentelemetry-collector/releases/download/cmd%2Fbuilder%2Fv0.88.0/ocb_0.88.0_darwin_amd64 
-mv ocb_0.88.0_darwin_amd64 ocb
-sudo chmod 777 ocb
+# MacOS
+wget -O ocb https://github.com/open-telemetry/opentelemetry-collector/releases/download/cmd%2Fbuilder%2Fv0.88.0/ocb_0.91.0_darwin_amd64 
+
+# Linux x64
+#wget -O ocb https://github.com/open-telemetry/opentelemetry-collector/releases/download/cmd%2Fbuilder%2Fv0.91.0/ocb_0.91.0_linux_amd64
+
+chmod +x ocb
 ./ocb --config builder-config.yaml
